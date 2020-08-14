38eb4abd3104369cb90ebb9f3ffdc0b73841517e
Basant-Elhussein <baasntelhussein@aucegypt.edu>
2020-08-14 02:05:27 +0200

    Creating package

---
+++ Day3/tasks/my_ws/build/my-pkg/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/basant/AUC_ROS/Day3/my_ws/src/my-pkg
+# Build directory: /home/basant/AUC_ROS/Day3/my_ws/build/my-pkg
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
